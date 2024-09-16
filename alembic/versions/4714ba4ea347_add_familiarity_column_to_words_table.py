f870f6addd15fee9381602582401f6f0562b8b33
Jason Wu <jason_wu@aai.com.tw>
2024-09-16 10:32:41 +0800

    feat(db): Add Alembic migration to create words table

---
+++ alembic/versions/4714ba4ea347_add_familiarity_column_to_words_table.py
@@ -0,0 +1,30 @@
+"""Add familiarity column to words table
+
+Revision ID: 4714ba4ea347
+Revises: e3e72c980409
+Create Date: 2024-09-16 10:05:07.308652
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "4714ba4ea347"
+down_revision: Union[str, None] = "e3e72c980409"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "words",
+        sa.Column("familiarity", sa.Integer(), nullable=False, server_default="0"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("words", "familiarity")
---
+++ alembic/versions/e3e72c980409_create_user_words.py
@@ -0,0 +1,48 @@
+"""create user words
+
+Revision ID: e3e72c980409
+Revises: 
+Create Date: 2024-09-12 11:35:34.173835
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
+
+
+# revision identifiers, used by Alembic.
+revision: str = "e3e72c980409"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    bind = op.get_bind()
+    inspector = Inspector.from_engine(bind)
+
+    if not inspector.has_table("user_words"):
+        op.create_table(
+            "words",
+            sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+            sa.Column("word", sa.Text, nullable=False),
+            sa.Column("cht", sa.Text, nullable=False),
+            sa.Column("mp3_url", sa.Text, nullable=False),
+            sa.Column(
+                "created_at", sa.DateTime, server_default=sa.func.now(), nullable=False
+            ),
+            sa.Column(
+                "updated_at",
+                sa.DateTime,
+                server_default=sa.func.now(),
+                onupdate=sa.func.now(),
+                nullable=False,
+            ),
+        )
+
+
+def downgrade() -> None:
+    op.drop_table("words")
