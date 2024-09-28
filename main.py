d046d04d0bacf92a37acc4beec2f0c4d2000e42f
Jason Wu <jasonwu070721@gmail.com>
2024-09-28 22:31:12 +0800

    feat(mp3): check and download MP3 when uploading duplicate word file

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ from controller.word import (
     increase_familiarity,
     get_all_words,
     get_input_file_words,
-    check_word_exists,
 )
 
 
@@ -42,66 +41,7 @@ from controller.input_file import (
 )
 
 import utils.config as config
-from retrieve_dictionary import (
-    get_dictionary_response,
-    get_tradionnal_chinese,
-    get_mp3_url_html,
-    save_mp3,
-    check_mp3_exists,
-)
-
-
-class FileProcessingThread(QThread):
-    progress_changed = pyqtSignal(int)
-
-    def __init__(self, file_path, input_file_id):
-        super().__init__()
-        self.file_path = file_path
-        self.input_file_id = input_file_id
-
-    def run(self):
-        try:
-            with open(self.file_path, "r", encoding="utf-8") as file:
-                lines = file.readlines()
-                total_lines = len(lines)
-                mp3_url = None
-                cht = None
-
-                for index, line in enumerate(lines):
-                    file_name_split = line.split(",")
-                    if len(file_name_split) >= 2:
-
-                        word = file_name_split[0].strip()
-
-                        if check_word_exists(word):
-                            print(str(word) + " already exists.")
-                            continue
-
-                        response = get_dictionary_response(word)
-
-                        if response != None:
-                            cht = get_tradionnal_chinese(response)
-                            print(str(word) + " ," + str(cht))
-
-                            mp3_url = get_mp3_url_html(response)
-                            if mp3_url is None:
-                                print("No MP3 URL found.")
-
-                            if not check_mp3_exists(word):
-                                save_mp3(mp3_url, word)
-
-                        else:
-                            print("No response.")
-
-                        if mp3_url != None and cht != None:
-                            print(word, cht, mp3_url, int(self.input_file_id))
-                            add_word(word, cht, mp3_url, int(self.input_file_id))
-
-                    progress = int((index + 1) / total_lines * 100)
-                    self.progress_changed.emit(progress)
-
-        except Exception as e:
-            print(f"Failed to read file: {e}")
+from utils.FileProcessingThread import FileProcessingThread
 
 
 class WordTableApp(QMainWindow):
---
+++ utils/FileProcessingThread.py
@@ -0,0 +1,75 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+
+from retrieve_dictionary import (
+    get_dictionary_response,
+    get_tradionnal_chinese,
+    get_mp3_url_html,
+    save_mp3,
+    check_mp3_exists,
+)
+
+from controller.word import (
+    add_word,
+    add_word,
+    check_word_exists,
+)
+
+
+class FileProcessingThread(QThread):
+    progress_changed = pyqtSignal(int)
+
+    def __init__(self, file_path, input_file_id):
+        super().__init__()
+        self.file_path = file_path
+        self.input_file_id = input_file_id
+
+    def run(self):
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                total_lines = len(lines)
+                mp3_url = None
+                cht = None
+                response = None
+
+                for index, line in enumerate(lines):
+                    file_name_split = line.split(",")
+                    if len(file_name_split) >= 2:
+
+                        word = file_name_split[0].strip()
+
+                        if not check_mp3_exists(word):
+                            save_mp3(mp3_url, word)
+
+                        if check_word_exists(word):
+                            print(str(word) + " already exists.")
+                            progress = int((index + 1) / total_lines * 100)
+                            self.progress_changed.emit(progress)
+
+                            continue
+
+                        response = get_dictionary_response(word)
+
+                        if response != None:
+                            cht = get_tradionnal_chinese(response)
+                            print(str(word) + " ," + str(cht))
+
+                            mp3_url = get_mp3_url_html(response)
+                            if mp3_url is None:
+                                print("No MP3 URL found.")
+
+                            if not check_mp3_exists(word):
+                                save_mp3(mp3_url, word)
+
+                        else:
+                            print("No response.")
+
+                        if mp3_url != None and cht != None:
+                            print(word, cht, mp3_url, int(self.input_file_id))
+                            add_word(word, cht, mp3_url, int(self.input_file_id))
+
+                    progress = int((index + 1) / total_lines * 100)
+                    self.progress_changed.emit(progress)
+
+        except Exception as e:
+            print(f"Failed to read file: {e}")
