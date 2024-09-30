aa119e59e311656a1242d2538bc4a11d2b4eaa84
Jason Wu <jason_wu@aai.com.tw>
2024-09-30 14:44:07 +0800

    fix(spider): add check for missing pronunciation links on translation websites

--- utils/FileProcessingThread.py
+++ utils/FileProcessingThread.py
@@ -28,37 +28,48 @@ class FileProcessingThread(QThread):
             with open(self.file_path, "r", encoding="utf-8") as file:
                 lines = file.readlines()
                 total_lines = len(lines)
-                mp3_url = None
-                cht = None
-                response = None
 
                 for index, line in enumerate(lines):
+                    mp3_url = None
+                    cht = None
+                    response = None
+
                     file_name_split = line.split(",")
                     if len(file_name_split) >= 2:
 
                         word = file_name_split[0].strip()
 
-                        if not check_mp3_exists(word):
-                            save_mp3(mp3_url, word)
-
                         if check_word_exists(word):
                             print(str(word) + " already exists.")
+
+                            if not check_mp3_exists(word):
+
+                                response = get_dictionary_response(word)
+                                mp3_url = get_mp3_url_html(response)
+                                if mp3_url is None:
+                                    print("No MP3 URL found.")
+
+                                if mp3_url != None and word != None:
+                                    save_mp3(mp3_url, word)
+
                             progress = int((index + 1) / total_lines * 100)
                             self.progress_changed.emit(progress)
 
                             continue
 
-                        response = get_dictionary_response(word)
+                        if response == None:
+                            response = get_dictionary_response(word)
 
                         if response != None:
                             cht = get_tradionnal_chinese(response)
                             print(str(word) + " ," + str(cht))
 
-                            mp3_url = get_mp3_url_html(response)
-                            if mp3_url is None:
-                                print("No MP3 URL found.")
+                            if mp3_url == None:
+                                mp3_url = get_mp3_url_html(response)
+                                if mp3_url is None:
+                                    print("No MP3 URL found.")
 
-                            if not check_mp3_exists(word):
+                            if not check_mp3_exists(word) and mp3_url != None:
                                 save_mp3(mp3_url, word)
 
                         else:
